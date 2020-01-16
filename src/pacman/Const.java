ee5af5d627d1b07da87671c0a072af35bf29bc72
antrobot1234 <antrobot1234@gmail.com>
2020-01-16 08:23:59 -0600

    Const.dirMap is now a hashmap of dirs and int arrays

--- src/pacman/Const.java
+++ src/pacman/Const.java
@@ -4,13 +4,13 @@ import java.util.HashMap;
 
 public class Const {
 	public enum Dir {left, right, up, down}
-	public static HashMap<Dir,Coord> dirMap = createMap();
-	public static HashMap<Dir,Coord> createMap(){
-		HashMap<Dir,Coord> map = new HashMap<>();
-		map.put(Dir.left, new Coord(-1,0));
-		map.put(Dir.right, new Coord(1,0));
-		map.put(Dir.up, new Coord(0,1));
-		map.put(Dir.down, new Coord(0,-1));
+	public static HashMap<Dir,int[]> dirMap = createMap();
+	public static HashMap<Dir,int[]> createMap(){
+		HashMap<Dir,int[]> map = new HashMap<>();
+		map.put(Dir.left, new int[]{-1, 0});
+		map.put(Dir.right, new int[]{1,0});
+		map.put(Dir.up, new int[]{0,1});
+		map.put(Dir.down, new int[]{0,-1});
 		return map;
 	}
 }
