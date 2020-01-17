5a6b038e3e14b423f9ac40772b471b10741523af
antrobot1234 <antrobot1234@gmail.com>
2020-01-17 10:01:02 -0600

    made keyworker give out a dir instead of booleans

--- src/pacman/objects/Entity.java
+++ src/pacman/objects/Entity.java
@@ -6,13 +6,20 @@ public class Entity {
 	public Coord pos;
 	public String type;
 	public Const.Dir dir;
+	public Const.Dir wantDir;
+	public boolean canBacktrack;
 	public int[] lookPos;
+	public boolean stunned;
 	public Entity(int x, int y,String type,Const.Dir direction,int size) {
+		stunned = false;
 		this.type = type;
 		pos = new Coord(x,y,size);
 		dir = direction;
+		wantDir = direction;
 		lookPos = null;
-		if(type.equals("pac")) lookPos = new int[]{x,y};
+		canBacktrack = false;
+		if(type.equals("pac"))canBacktrack = true;
+		else lookPos = new int[]{x,y};
 	}
 	public void step() {
 		pos.sum(Const.dirMap.get(dir));
--- src/pacman/util/KeyWorker.java
+++ src/pacman/util/KeyWorker.java
@@ -1,5 +1,7 @@
 package pacman.util;
 
+import pacman.util.Const.Dir;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.HashMap;
@@ -7,16 +9,16 @@ import java.util.HashMap;
 public class KeyWorker implements KeyListener {
     public boolean[] keys;
     public HashMap<String,Boolean> move;
-    public boolean up, down, left, right;
+    public Dir point;
     public KeyWorker(){
         keys = new boolean[256];
     }
 
     public void update(){
-        up = keys[KeyEvent.VK_W];
-        down = keys[KeyEvent.VK_S];
-        left = keys[KeyEvent.VK_A];
-        right = keys[KeyEvent.VK_D];
+        if(keys[KeyEvent.VK_W])point = Dir.up;
+        else if(keys[KeyEvent.VK_S])point = Dir.down;
+        else if(keys[KeyEvent.VK_A])point = Dir.left;
+        else if(keys[KeyEvent.VK_D])point = Dir.right;
     }
 
     public void keyPressed(KeyEvent e) {
